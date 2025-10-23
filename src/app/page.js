f8ebb7b072df7810c66c735c872f86d297a340ce
duttachandan <duttaankit917@gmail.com>
2025-10-23 20:13:36 +0530

    update banner content

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,31 @@ import Image from "next/image";
 import Link from "next/link";
 import home from "../styles/home.module.css"
 
+// Profiles 
+import Profile1 from "../assets/profile1.png"
+import Profile2 from "../assets/profile2.png"
+import Profile3 from "../assets/profile3.png"
+import Profile4 from "../assets/profile4.png"
+
 export default function Home() {
+  const profilesOfMembers = [
+    {
+      profile: Profile1,
+      alternteText: "Profiles of Members",
+    },
+    {
+      profile: Profile2,
+      alternteText: "Profiles of Members",
+    },
+    {
+      profile: Profile3,
+      alternteText: "Profiles of Members",
+    },
+    {
+      profile: Profile4,
+      alternteText: "Profiles of Members",
+    }
+  ]
   return (
     <>
       <main>
@@ -11,12 +35,33 @@ export default function Home() {
           flex items-end`}
         >
           <div className="container">
-            <div className="row">
+            <div className={`row ${home.banner_row}`}>
               <div className="col-4">
                 {/* Members */}
-                <p className="grad-text">Lorem ipsum dolor sit amet consectetur. Nullam vitae leo sed scelerisque.</p>
+                <p
+                  className="grad-text"
+                >Lorem ipsum dolor sit amet consectetur.
+                  Nullam vitae leo sed scelerisque.
+                </p>
+                <div className={`${home.memebers}`}>
+                  {
+                    profilesOfMembers.map((data, index) => (
+                      <Image key={index} src={data.profile} alt={data.alternteText} />
+                      )
+                    )
+                  }
+                </div>
+                <div className="grad-text">
+                  <span className="primary-text">12k +</span> Membership Enjoy Our Programs
+                </div>
               </div>
-              <div className="col-8"></div>
+              <div className="col-8">
+              <h1 className="title1">
+                FocusFuel
+                <br />
+                <span className="primary-text">Fitness</span>
+              </h1>
+              </div>
             </div>
           </div>
         </section>
