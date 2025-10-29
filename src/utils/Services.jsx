eaefe04086572c5740e3e11f34b9b6a96f6a1ad5
duttachandan <duttaankit917@gmail.com>
2025-10-29 13:36:07 +0530

    update slider

--- src/utils/Services.jsx
+++ src/utils/Services.jsx
@@ -3,88 +3,73 @@ import React from "react";
 import home from "@/styles/home.module.css";
 import Image from "next/image";
 
-// Slick Carousel
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+// Splide Carousel
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css"; // Default theme (you can switch to /core or /skyblue)
 
-// Personal Traning
+// Personal Training Images
 import Trainer from "../assets/Trainer1.png";
 import Trainer2 from "../assets/Trainer2.png";
 import Trainer3 from "../assets/Trainer3.png";
 
 const Services = () => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-        },
-      }
-    ],
+  const options = {
+    type: "loop",
+    perPage: 3,
+    autoplay: true,
+    interval: 2000,
+    pagination: false,
+    arrows: false,
+    breakpoints: {
+      1024: { perPage: 2 },
+      576: { perPage: 1 },
+    },
   };
+
+  const trainers = [
+    {
+      img: Trainer,
+      title: "Personal Training",
+      count: "01",
+    },
+    {
+      img: Trainer2,
+      title: "Group Workout",
+      count: "02",
+    },
+    {
+      img: Trainer3,
+      title: "Muscle Building",
+      count: "03",
+    },
+  ];
 
   return (
     <section className="services-sec">
       <div className={`container ${home.slick_container}`}>
-        <Slider {...settings}>
-          <div className={home.slider_card}>
-            <div className={`${home.services_card}`}>
-              <h2 className={`title2 ${home.services_title}`}>
-                Personal Traning
-              </h2>
-              <div className={home.services_count}>01</div>
-              <div className={home.services_img}>
-                <Image src={Trainer} alt="Trainer No 1" />
+        <Splide options={options} aria-label="Services Carousel">
+          {trainers.map((img, i) => (
+            <SplideSlide key={i}>
+              <div className={home.slider_card}>
+                <div className={home.services_card}>
+                  <h2 className={`title2 ${home.services_title}`}>
+                    {img.title}
+                  </h2>
+                  <div className={home.services_count}>{img.count}</div>
+                  <div className={home.services_img}>
+                    <Image
+                      src={img.img}
+                      alt={`Trainer No ${i + 1}`}
+                      width={300}
+                      height={300}
+                      priority={i === 0} // Optimize first image
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className={home.slider_card}>
-            <div className={`${home.services_card}`}>
-              <h2 className={`title2 ${home.services_title}`}>
-                Personal Traning
-              </h2>
-              <div className={home.services_count}>01</div>
-              <div className={home.services_img}>
-                <Image src={Trainer} alt="Trainer No 1" />
-              </div>
-            </div>
-          </div>
-          <div className={home.slider_card}>
-            <div className={`${home.services_card}`}>
-              <h2 className={`title2 ${home.services_title}`}>
-                Personal Traning
-              </h2>
-              <div className={home.services_count}>01</div>
-              <div className={home.services_img}>
-                <Image src={Trainer} alt="Trainer No 1" />
-              </div>
-            </div>
-          </div>
-          <div className={home.slider_card}>
-            <div className={`${home.services_card}`}>
-              <h2 className={`title2 ${home.services_title}`}>
-                Personal Traning
-              </h2>
-              <div className={home.services_count}>01</div>
-              <div className={home.services_img}>
-                <Image src={Trainer} alt="Trainer No 1" />
-              </div>
-            </div>
-          </div>
-        </Slider>
+            </SplideSlide>
+          ))}
+        </Splide>
       </div>
     </section>
   );
