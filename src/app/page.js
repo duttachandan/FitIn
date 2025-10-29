88012cbde54f8cff3496d3a158f12676f979bb96
duttachandan <duttaankit917@gmail.com>
2025-10-29 17:25:12 +0530

    updated half about section

--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,9 @@ import Profile2 from "../assets/profile2.png";
 import Profile3 from "../assets/profile3.png";
 import Profile4 from "../assets/profile4.png";
 
+import eclipse from "@/assets/Eclipse.png";
+import About from "@/assets/About.png";
+
 export default async function Page() {
   await new Promise((resolve) => setTimeout(resolve, 3000));
 
@@ -95,6 +98,51 @@ export default async function Page() {
 
         {/*Services Provided Section */}
         <Services />
+
+        {/* About Our Gym Section */}
+        <section className={`${home.about_sec}`}>
+          <div className="container">
+            <div className={`row ${home.about_heading}`}>
+              <div className="col-6">
+                <h2 className={`title2 ${home.about_title}`}>About Our Gym</h2>
+              </div>
+              <p className="grad-text col-6">
+                Empowering Every Body to Move Stronger, Live Better, and Go
+                Further
+              </p>
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <div className={home.about_img}>
+                  <Image src={About} alt="" />
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="About-content">
+                  <h3 className={home.about_subTitle}>
+                    We believe fitness is more than just physical. It’s about
+                    building confidence, discipline, and a lifestyle you can be
+                    proud of.
+                  </h3>
+                  <div className="row">
+                    <div className="col-6">
+                      Founded with the version of creating a spacet for all
+                      fitness levels .Our gym offers expert guidence modern
+                      equipment and an uplifting atmosphere to help you achieve
+                      your goals.
+                    </div>
+                    <div className="col-6">
+                      Whether you’re a beginner or a seasoned athlete. We’re
+                      here to support your journey with personalized training,
+                      diverse programs and a community that motivates you every
+                      step of the way
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   );
